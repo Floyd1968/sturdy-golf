f168abae79212f43bb93314622e9a0b730890490
administrator <administrator@DLX>
2018-07-12 04:05:29 -0700

    Hello this is not my world

--- Hello/src/Hello/Program.cs
+++ Hello/src/Hello/Program.cs
@@ -9,7 +9,7 @@ namespace Hello
     {
         public static void Main(string[] args)
         {
-            Console.Write("Hello my world");
+            Console.Write("Hello this is not my world");
 
         }
     }
